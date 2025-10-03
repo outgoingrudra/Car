132def5004e4b572b80277e0491073dfea8d2308
Rudra Verma <rudraverma781@gmail.com>
2025-10-03 17:58:44 +0530

    Changes made

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {assets} from "../assets/assets"
+import {assets, menuLinks} from "../assets/assets"
 import {Link} from "react-router-dom"
 
 export default function Navbar() {
@@ -9,6 +9,17 @@ export default function Navbar() {
       <Link to='/'>
          <img src={assets.logo} alt="logo"  className='h-8' />    
       </Link>
+      <div className="max-sm:fixed max-sm:h-screen max-sm:w-full  max-sm:top-16 max-sm:border-t border-borderColor right-0 flex  
+      flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50">
+
+        {
+          menuLinks.map((link,index)=>(
+            <Link key={index} to={link.path}>
+             {link.name}
+            </Link>
+          ))
+        }
+      </div>
 
 
 
