66106587911011a57f7ad47c1f6b818a660167a2
Rudra Verma <rudraverma781@gmail.com>
2025-10-03 07:53:55 +0530

    Changes made

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import {assets} from "../assets/assets"
+import {Link} from "react-router-dom"
 
 export default function Navbar() {
   return (
     <div>
+      
+      <Link to='/'>
+         <img src={assets.logo} alt="logo"  className='h-8' />    
+      </Link>
 
 
-        
+
     </div>
   )
 }
