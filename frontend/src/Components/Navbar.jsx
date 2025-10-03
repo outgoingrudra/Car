d7190fbfcd8924c0260cd497369de7eb342f86ec
Rudra Verma <rudraverma781@gmail.com>
2025-10-03 18:18:09 +0530

    Changes made

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets, menuLinks} from "../assets/assets"
-import {Link} from "react-router-dom"
+import {Link, useLocation} from "react-router-dom"
 
 export default function Navbar() {
+
+  const location =  useLocation()
+  const [open,setOpen] =  useState(false)
   return (
     <div>
       
       <Link to='/'>
          <img src={assets.logo} alt="logo"  className='h-8' />    
       </Link>
-      <div className="max-sm:fixed max-sm:h-screen max-sm:w-full  max-sm:top-16 max-sm:border-t border-borderColor right-0 flex  
-      flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50">
+      <div className={`max-sm:fixed max-sm:h-screen max-sm:w-full  max-sm:top-16 max-sm:border-t border-borderColor right-0 flex  
+      flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50 ${location.pathname=="/" ? "bg-light" : "bg-white"} ${
+        open ? "max-sm:translate-x-0" : "max-translate-x-full"
+        
+      }`}>
 
         {
           menuLinks.map((link,index)=>(
