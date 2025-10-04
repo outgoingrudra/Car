a7740d1ee62f661258f312566ed7c7a9ad4c42cc
Rudra Verma <rudraverma781@gmail.com>
2025-10-04 13:22:55 +0530

    Changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar'
+import { useLocation } from 'react-router-dom'
 
 export default function App() {
-  return (
-    <div>
-     <Navbar/>
 
-    </div>
+
+    const [showLogin , setShowLogin ] = useState(false)
+
+    const isOwnerPath =  useLocation().pathname.startsWith('/owner')
+
+  return (
+    <>
+    {!isOwnerPath &&  <Navbar setShowLogin={setShowLogin}/> }
+
+    </>
   )
 }
