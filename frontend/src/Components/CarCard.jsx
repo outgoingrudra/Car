dc45f3f7e8fc380cc6149ec7a7dfc2558ddf2dc0
Rudra Verma <rudraverma781@gmail.com>
2025-10-05 09:49:04 +0530

    Changes made

--- frontend/src/Components/CarCard.jsx
+++ frontend/src/Components/CarCard.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 
 export default function CarCard({car}) {
+
+    const currency = import.meta.env.VITE_CURRENCY
+
   return (
     <div className='group rounded-xl overflow-hidden shadow-lg hover:translate-y-1 transition-all duration-500 cursor-pointer '>
-        <div className="">
+        <div className="relative h-48 overflow-hidden">
             <img src={car.image} alt="car image " className='w-full h-full object-cover transition-transform duration-500 group-hover:scale-105' />
             {
                 car.isAvailable && <p className='absolute top-4 left-4 bg-primary/90 text-white text-xs px-2.5 py-1 rounded-full '>Available Now </p>
             }
 
             <div className="absolute bottom-4 right-4 bg-black/80 backdrop-blur-sm text-white px-3 py-2 rounded-lg ">
-                <span>${car.pricePerDay}</span>
-                <span></span>
+                <span className='font-semibold'>{currency}{car.pricePerDay}</span>
+                <span className='text-sm text-white/80'> / day</span>
             </div>
         </div>
 
-
+           <div className="">
+            
+           </div>
     </div>
   )
 }
