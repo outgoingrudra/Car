d4bc84b5b875cd213dc69123895690779b65d282
Rudra Verma <rudraverma781@gmail.com>
2025-10-05 10:01:37 +0530

    Changes made

---
+++ frontend/src/Components/FeaturedSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function FeaturedSection() {
+  return (
+    
+    <div className="    flex flex-col items-center py-24 px-6 md:px-16 lg:px-24  xl:px-32">
+
+        <div className="">
+            
+        </div>
+
+    </div>
+  )
+}
