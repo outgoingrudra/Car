02990fcad2f13dd76c0f956a5cbab56b41cb5535
Rudra Verma <rudraverma781@gmail.com>
2025-10-06 10:53:37 +0530

    Changes made

--- frontend/src/Components/Testimonial.jsx
+++ frontend/src/Components/Testimonial.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import Title from './Title'
 
 export default function Testimonial() {
+
+     const testimonials = [
+        {   name: "Emma Rodriguez", 
+            location: "Barcelona, Spain",
+             image: "", 
+             rating: 5, 
+             testimonial: "Exceptional service and attention to detail. Everything was handled professionally and efficiently from start to finish. Highly recommended!" },
+        {  name: "Liam Johnson", address: "New York, USA", image: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200", rating: 4, review: "I’m truly impressed by the quality and consistency. The entire process was smooth, and the results exceeded all expectations. Thank you!" },
+        {   name: "Sophia Lee", address: "Seoul, South Korea", image: "https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=200", rating: 5, review: "Fantastic experience! From start to finish, the team was professional, responsive, and genuinely cared about delivering great results." }
+    ];
+
   return (
    
      <div className="py-28 px-6 md:px-16 lg:px-24  xl:px-44">
         <Title title="What our customers say  " subTitle="Discover why discrening travelers choose stayVenture  for their luxury accomodations around the world "  />
            
 
-            <div className="">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-18">
                 {testimonials.map((testimonial) => (
                     <div key={testimonial.id} className="bg-white p-6 rounded-xl shadow max-w-xs">
                         <div className="flex items-center gap-3">
