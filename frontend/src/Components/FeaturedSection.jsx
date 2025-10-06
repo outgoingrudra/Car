f0e53d46977044b6a075bf79486bc1edb0fa34d8
Rudra Verma <rudraverma781@gmail.com>
2025-10-06 10:26:10 +0530

    Changes made

--- frontend/src/Components/FeaturedSection.jsx
+++ frontend/src/Components/FeaturedSection.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Title from './Title'
+import {dummyCarData} from "../assets/assets"
+import CarCard from "../Components/CarCard"
 
 export default function FeaturedSection() {
   return (
@@ -6,8 +9,23 @@ export default function FeaturedSection() {
     <div className="    flex flex-col items-center py-24 px-6 md:px-16 lg:px-24  xl:px-32">
 
         <div className="">
-            
+            <Title title ="Featured Vehicles " subTitle="explore our selection of premium  vehicles available for your next adventure "  />
         </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-18">
+
+          {
+            dummyCarData.slice(0,6).map((car)=>(
+              <div className="" key={car._id}>
+                <CarCard car={car} />
+              </div>
+            ))
+          }
+
+        </div>
+        <button>
+          Ex
+        </button>
 
     </div>
   )
