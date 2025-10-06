31338a70cba92fd7cb09dde6aed6981ff757ddc0
Rudra Verma <rudraverma781@gmail.com>
2025-10-06 10:18:50 +0530

    Changes made

---
+++ frontend/src/Components/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Title({title , subTitle , align }) {
+  return (
+    <div className={`flex flex-col justify-center items-center text-center ${align == "left" && "md:items-start md:text-left"} `}>
+
+
+
+
+        
+    </div>
+  )
+}
