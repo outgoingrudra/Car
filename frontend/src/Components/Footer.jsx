782490a3014b4a242fd4e6a8639130f7134df838
Rudra Verma <rudraverma781@gmail.com>
2025-10-07 19:42:35 +0530

    Changes made

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -20,40 +20,18 @@ export default function Footer() {
                 </div>
 
                 <div>
-                    <p className='text-lg text-gray-800'>COMPANY</p>
+                    <p className='text-base font-medium  uppercase text-gray-800'>Quick Links </p>
                     <ul className='mt-3 flex flex-col gap-2 text-sm'>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Browse Cars</a></li>
+                        <li><a href="#">List your car</a></li>
                         <li><a href="#">About</a></li>
-                        <li><a href="#">Careers</a></li>
-                        <li><a href="#">Press</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">Partners</a></li>
+                      
                     </ul>
                 </div>
 
-                <div>
-                    <p className='text-lg text-gray-800'>SUPPORT</p>
-                    <ul className='mt-3 flex flex-col gap-2 text-sm'>
-                        <li><a href="#">Help Center</a></li>
-                        <li><a href="#">Safety Information</a></li>
-                        <li><a href="#">Cancellation Options</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                        <li><a href="#">Accessibility</a></li>
-                    </ul>
-                </div>
-
-                <div className='max-w-80'>
-                    <p className='text-lg text-gray-800'>STAY UPDATED</p>
-                    <p className='mt-3 text-sm'>
-                        Subscribe to our newsletter for inspiration and special offers.
-                    </p>
-                    <div className='flex items-center mt-4'>
-                        <input type="text" className='bg-white rounded-l border border-gray-300 h-9 px-3 outline-none' placeholder='Your email' />
-                        <button className='flex items-center justify-center bg-black h-9 w-9 aspect-square rounded-r'>
-                            {/* Arrow icon */}
-                            <svg className="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 12H5m14 0-4 4m4-4-4-4" /></svg>
-                        </button>
-                    </div>
-                </div>
+                
+                
             </div>
             <hr className='border-gray-300 mt-8' />
             <div className='flex flex-col md:flex-row gap-2 items-center justify-between py-5'>
