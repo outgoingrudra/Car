c028198569689bf96532a7dfa7ae61ef85bca53c
Rudra Verma <rudraverma781@gmail.com>
2025-10-08 17:21:50 +0530

    Changes made

--- frontend/src/Pages/CarDetails.jsx
+++ frontend/src/Pages/CarDetails.jsx
@@ -1,6 +1,6 @@
 import React, { use, useEffect, useState } from 'react'
 import {useNavigate, useParams } from "react-router-dom"
-import {dummyCarData} from "../assets/assets"
+import {assets, dummyCarData} from "../assets/assets"
 
 export default function CarDetails() {
   const {id} = useParams()
@@ -13,7 +13,19 @@ export default function CarDetails() {
   },[id])
   return   (
     car ?
-    <div className=''>
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32 mt-16'>
+      <button onClick={()=>navigate(-1)} className='flex items-center gap-2 mb-6 text-gray-600  cursor-pointer '>
+        <img src={assets.arrow_icon} alt="" className='rotate-180 opacity-65 '/>
+        Back to All cars
+      </button>
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-12 ">
+
+        {/* car image and details */}
+
+        
+
+      </div>
 
 
 
