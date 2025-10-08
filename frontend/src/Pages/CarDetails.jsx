8ae3e35fbc274e274971951419ccf80739eca16a
Rudra Verma <rudraverma781@gmail.com>
2025-10-08 17:17:42 +0530

    Changes made

--- frontend/src/Pages/CarDetails.jsx
+++ frontend/src/Pages/CarDetails.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { use, useEffect, useState } from 'react'
+import {useNavigate, useParams } from "react-router-dom"
+import {dummyCarData} from "../assets/assets"
 
 export default function CarDetails() {
-  return (
-    <div>CarDetails</div>
+  const {id} = useParams()
+  const navigate = useNavigate()
+
+  const [car , setCar] = useState(null)
+
+  useEffect(()=>{
+    setCar(dummyCarData.find(car =>car._id = id))
+  },[id])
+  return   (
+    car ?
+    <div className=''>
+
+
+
+    </div>
+    : <p>Loading ......</p>
   )
 }
