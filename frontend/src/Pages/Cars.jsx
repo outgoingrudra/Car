e08931dcef396331fabe45d3a3ee1aaa95b9eb5e
Rudra Verma <rudraverma781@gmail.com>
2025-10-10 17:42:26 +0530

    Changes made

--- frontend/src/Pages/Cars.jsx
+++ frontend/src/Pages/Cars.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import Title from '../Components/Title'
 
 export default function Cars() {
   return (
-    <div>Cars</div>
+    <div className=''>
+
+      <div className="flex flex-col items-center py-20 bg-light max-md:px-4 ">
+        <Title title="Available Cars "  subTitle="Browse Our Selection of premium vehicles available for you next adventure " />
+
+      </div>
+
+      <div className="
+      ">
+
+      </div>
+    </div>
   )
 }
