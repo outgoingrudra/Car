bad0bd862a26aea5d86591ba4e23deca29226e95
Rudra Verma <rudraverma781@gmail.com>
2025-10-11 07:46:33 +0530

    Changes made

--- frontend/src/Pages/MyBookings.jsx
+++ frontend/src/Pages/MyBookings.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { dummyMyBookingsData } from '../assets/assets'
+import Title from '../Components/Title'
 
 export default function MyBookings() {
+
+  const [bookings , setBookings] = React.useState([])
+
+  const fetchBookings = async()=>{
+
+    setBookings(dummyMyBookingsData)
+  }
+
+  useEffect(()=>{
+    fetchBookings()
+  },[])
   return (
-    <div>MyBookings</div>
+    <div className='px-6 md:px-16 lg:px-24  xl:px-32  2xl:px-48  mt-16  text-sm max-w-7xl'>
+
+
+     <Title title="My Bookings " subTitle=" View and manage your all car bookings "/>
+
+    </div>
   )
 }
