ed55d37f4f6a505ddb44b1d72f05a4c9ef97238c
Rudra Verma <rudraverma781@gmail.com>
2025-10-20 15:54:12 +0530

    Changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,11 @@ import Home from "../src/Pages/Home"
 import Cars from "../src/Pages/Cars"
 import CarDetails from './Pages/CarDetails'
 import MyBookings from './Pages/MyBookings'
+import Layout from './Pages/Owner/Layout'
+import Dashboard from './Pages/Owner/Dashboard'
+import AddCar from './Pages/Owner/AddCar'
+import ManageCars from './Pages/Owner/ManageCars'
+import ManageBookings from './Pages/Owner/ManageBookings'
 
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Footer from './Components/Footer'
@@ -23,6 +28,13 @@ export default function App() {
       <Route path='/car-details/:id' element={<CarDetails/>}/>
       <Route path='/cars' element={<Cars/>}/>
       <Route path='/my-bookings' element={<MyBookings/>}/>
+      <Route path='/owner' element={<Layout />}> 
+        <Route index element={<Dashboard/>}/>
+        <Route path='add-car' element={<AddCar/>}/>
+        <Route path='manage-cars' element={<ManageCars/>}/>
+        <Route path='manage-bookings' element={<ManageBookings/>}/>
+
+      </Route>
     </Routes>
   {!isOwnerPath &&  <Footer/> }
 
