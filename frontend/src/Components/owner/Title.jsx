1f6d4639be4db514a8dba59f2b38834e590d3f33
Rudra Verma <rudraverma781@gmail.com>
2025-10-20 15:58:46 +0530

    Changes made

--- frontend/src/Components/owner/Title.jsx
+++ frontend/src/Components/owner/Title.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-export default function Title() {
+export default function Title({title , subTitle}) {
+    
   return (
-    <div>Title</div>
+   <>
+     <h1 className="font-medium text-3xl">{title}</h1>
+     <p className="text-sm md:text-base text-gray-500/90 mt-2 max-w-156 ">{subTitle}</p>
+   </>
   )
 }
