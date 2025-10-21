bc8b7da1ee9ffd364bde4331646c097a1c243cf6
Rudra Verma <rudraverma781@gmail.com>
2025-10-21 15:52:49 +0530

    Changes made

--- frontend/src/Components/owner/Sidebar.jsx
+++ frontend/src/Components/owner/Sidebar.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { dummyUserData } from '../../assets/assets';
+import { useLocation } from 'react-router-dom';
+
 
 export default function Sidebar() {
+  const user = dummyUserData ;
+  const location = useLocation();
+  const [image , setImage] = React.useState('');
+  const updateImage =async  () => {
+    user.image = URL.createObjectURL(image);
+    setImage('')
+
+
   return (
     <div>Sidebar</div>
   )
