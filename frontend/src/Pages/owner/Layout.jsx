b3cb2fac095d131b1329425aa249b0335834630b
Rudra Verma <rudraverma781@gmail.com>
2025-11-06 09:38:07 +0530

    Changes made

--- frontend/src/Pages/owner/Layout.jsx
+++ frontend/src/Pages/owner/Layout.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import NavbarOwner from '../../Components/owner/NavbarOwner'
+import Sidebar from '../../Components/owner/Sidebar'
+import { Outlet } from 'react-router-dom'
 
 export default function Layout() {
   return (
-    <div>Layout</div>
+    <div className='flex flex-col '>
+      <NavbarOwner />
+      <div className="">
+        <Sidebar />
+        <Outlet/>
+      </div>
+
+
+    </div>
   )
 }
