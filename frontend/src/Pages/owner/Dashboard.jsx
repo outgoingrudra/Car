bebbefae34f97cffdfe483ea883486cfe74f774f
Rudra Verma <rudraverma781@gmail.com>
2025-11-07 17:38:32 +0530

    Changes made

--- frontend/src/Pages/owner/Dashboard.jsx
+++ frontend/src/Pages/owner/Dashboard.jsx
@@ -29,7 +29,29 @@ export default function Dashboard() {
   return (
    <div className="px-4 pt-10 md:px-10 flex-1 ">
     <Title title="Admin Dashboard"  subTitle="Monitor overall platform performance including total cars , bookings , revenue and recent activites" />
+     
 
+
+     <div className="grid  sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 my-8 max-w-3xl ">
+            {
+              dashboardCards.map((card , index) => (
+                     <div  key={index} className="flex gap-2 items-center justify-between p-4 
+                     rounded-md border border-borderColor">
+
+                      <div className="">
+                        <h1 className="text-xs text-gray-500">{card.title}</h1>
+                        <p className="text-lg font-semibold">{card.value}</p>
+                      </div>
+                      <div className="flex items-center justify-center w-10 h-10 rounded-full bg-primary/10">
+                        
+                        <img src={card.icon} className='h-4 w-4 ' alt="" />
+
+                      </div>
+
+                     </div>
+              ))
+            }
+     </div>
     
    </div>
   )
