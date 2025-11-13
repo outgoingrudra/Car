25441d60d21b6f2758fe268c7d213e42c962986f
Rudra Verma <rudraverma781@gmail.com>
2025-11-13 11:06:07 +0530

    Changes made

--- frontend/src/Pages/owner/AddCar.jsx
+++ frontend/src/Pages/owner/AddCar.jsx
@@ -117,8 +117,25 @@ export default function AddCar() {
                 <input type="number" placeholder='4' required className='px-3 py-2 
                 mt-1  border border-borderColor rounded-md outline-none' value={car.seating_capacity} onChange={(e)=>setCar({...car,seating_capacity:e.target.value})} />
               </div>
+             </div>
+             {/* car location */}
 
-              
+             <div className="flex flex-col w-full">
+
+               <div className="flex flex-col w-full">
+                 <label htmlFor="">Location</label>
+                 <select name="" id=""  
+                 className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'
+                 onChange={(e)=>setCar({...car,location :e.target.value})} value={car.location}>
+                  <option value="">Select a location</option>
+                  <option value="New York">New York</option>
+                  <option value="Los Angeles">Los Angeles</option>
+                  <option value="Houstan">Houstan</option>
+                  <option value="Chicago">Chicago</option>
+                  
+                 </select>
+              </div>
+              {/* car description */}
 
              </div>
 
