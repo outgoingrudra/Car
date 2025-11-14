e36834e7b26b1549360e0af707545cf27d3cc84a
Rudra Verma <rudraverma781@gmail.com>
2025-11-14 10:05:24 +0530

    Changes made

--- frontend/src/Pages/owner/ManageCars.jsx
+++ frontend/src/Pages/owner/ManageCars.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import {dummyCarData} from "../../assets/assets"
+import Title from "../../Components/owner/Title"
 
 export default function ManageCars() {
 
@@ -14,7 +15,23 @@ export default function ManageCars() {
   },[])
   return (
    <div className="px-4 pt-10 md:px-10 w-full">
+    <Title title="Manage Cars" subTitle="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, voluptatibus!"/>
+    
+    <div className="max-w-3xl w-full rounded-md overflow-hidden border border-borderColor mt-6 ">
+      <table className='w-full border-collapse text-left text-sm text-gray-600'>
+        <thead className='text-gray-500'>
+          <tr>
+            <th className='p-3 font-medium'>Car</th>
+            <th className='p-3 font-medium max-md:hidden'>Category</th>
+            <th className='p-3 font-medium'>price</th>
+            <th className='p-3 font-medium max-md:hidden'>Status </th>
+            <th className='p-3 font-medium'>Actions</th>
+          </tr>
+        </thead>
 
+      </table>
+
+    </div>
    </div>
   )
 }
