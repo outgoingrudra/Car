3a8dc76eba7ec2b3ccd0d27f7b75fdd1615a8255
Rudra Verma <rudraverma781@gmail.com>
2025-12-03 11:58:12 +0530

    Changes made

---
+++ backend/configs/db.js
@@ -0,0 +1,17 @@
+import mongoose  from "mongoose";   
+
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', () => {
+            console.log("MongoDB connected successfully");
+        });
+        await mongoose.connect(`${rocess.env.MONGODB_URI}/car-rental`)
+            
+    }
+    catch (error) {
+        console.log("Error connecting to database", error.message);
+    }
+}
+
+export default connectDB;
