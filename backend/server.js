e82b89a5170306fd849d4e5e898e62a1660734ae
Rudra Verma <rudraverma781@gmail.com>
2025-12-16 08:20:42 +0530

    Changes made

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import "dotenv/config"
 import cors from "cors"
 import connectDB from "./configs/db.js"
 import userRouter from "./Routes/userRoutes.js"
+import ownerRouter from "./Routes/ownerRoutes.js"
 
 
 const app = express()
@@ -18,6 +19,7 @@ app.get("/",(req,res)=>{
     res.send("Server is running ")
 })
 app.use("/api/users",userRouter)
+app.use("/api/owner",ownerRouter)
 
 
 const PORT = process.env.PORT  || 3000 ;
