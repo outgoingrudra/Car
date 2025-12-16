0fd8ca3377d975be696380b448a4d5875e4da20f
Rudra Verma <rudraverma781@gmail.com>
2025-12-16 09:18:32 +0530

    Changes made

--- backend/Routes/ownerRoutes.js
+++ backend/Routes/ownerRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { changeRoleToOwner } from "../controllers/ownerController.js";
 import {protect} from "../middleware/auth.js"
+import upload from "../middleware/multer.js"
+import { addCar } from "../controllers/ownerController.js";
 const ownerRouter = express.Router();
 
 ownerRouter.post("/change-role",protect , changeRoleToOwner);
+ownerRouter.post("/add-car",upload.single("image"), addCar);
 
 export default ownerRouter;
--- backend/controllers/ownerController.js
+++ backend/controllers/ownerController.js
@@ -1,4 +1,7 @@
 import User from "../models/user.js"
+import fs from 'fs';
+import imagekit from "../configs/imagekit.js"
+import Car from "../models/Car.js"
 export const changeRoleToOwner = async (req, res) => {
     try {
         const {_id } = req.user;
@@ -17,6 +20,33 @@ export const addCar = async (req, res) => {
         const {_id } = req.user;
         let car = JSON.parse(req.body.carData);
         const imageFile = req.file;
+        // upload image to image kit
+        const fileBuffer = fs.readFileSync(imageFile.path);
+        const response = await imagekit.upload({
+            file : fileBuffer, 
+            fileName : imageFile.originalname,
+            folder: "/cars"
+        })
+        var optimizedImageUrl = imagekit.url({
+            path: response.filePath,
+            transformation: [
+                {
+                    width : "1280",  
+                },
+                {quality  : "auto"}
+                ,{format  : "webp"}
+            ]
+        });
+        const image = optimizedImageUrl;
+        await Car.create({
+            ...car,
+            image,
+            owner: _id
+        })
+        res.json({success:true , message: 'Car added successfully'
+        })
+        
+
 
         
     } catch (error) {
