73d871d47c1650f9cd1720399f3d217276f44975
Rudra Verma <rudraverma781@gmail.com>
2025-12-16 08:57:04 +0530

    Changes made

---
+++ backend/configs/imagekit.js
@@ -0,0 +1,10 @@
+import ImageKit from "imagekit";
+
+var imagekit = new ImageKit({
+    publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT
+});
+
+console.log("Imagekit configured");
+export default imagekit;
