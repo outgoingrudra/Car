a199b3a41f72c9c00494b15ab541d4257754b2d1
Rudra Verma <rudraverma781@gmail.com>
2026-01-01 08:32:04 +0530

    Changes made

--- frontend/src/Pages/owner/ManageBookings.jsx
+++ frontend/src/Pages/owner/ManageBookings.jsx
@@ -1,14 +1,45 @@
 import React, { useEffect, useState } from 'react'
-import {dummyMyBookingsData} from "../../assets/assets"
+
 import Title from "../../Components/owner/Title"
+import { useAppContext } from "../../Context/AppContext";
+import toast from "react-hot-toast";
 
 export default function ManageBookings() {
-  const [bookings ,SetBookings ] = useState([])
-      const currency = import.meta.env.VITE_CURRENCY ; 
+    const {axios,currency } = useAppContext()
+
+    const [bookings ,SetBookings ] = useState([])
+      
 
 
   const fetchOwnerBookings = async()=>{
-    SetBookings(dummyMyBookingsData)
+       try {
+        const {data} = await axios.get("/api/bookings/owner")
+        if(data.success)
+        {
+          SetBookings(data.bookings)
+        }
+        else {
+          toast.error(data.message)
+        }
+       } catch (error) {
+         toast.error(error.message)
+       }
+  }
+
+   const changeBookingStatus = async(bookingId,status)=>{
+       try {
+        const {data} = await axios.post("/api/bookings/change-status",{bookingId,status})
+        if(data.success)
+        {
+        toast.success(data.message)
+        fetchOwnerBookings()
+        }
+        else {
+          toast.error(data.message)
+        }
+       } catch (error) {
+         toast.error(error.message)
+       }
   }
 
   useEffect(()=>{
@@ -53,7 +84,7 @@ export default function ManageBookings() {
                     </td>
                     <td className="p-3">
                          {booking.status == "pending" ? (
-                          <select value={booking.status} className='px-2 py-1.5 mt-1 text-gray-500 border border-borderColor rounded-md outline-none '> 
+                          <select onChange={(e)=>changeBookingStatus(booking._id,e.target.value)} value={booking.status} className='px-2 py-1.5 mt-1 text-gray-500 border border-borderColor rounded-md outline-none '> 
                           <option value="pending">pending</option>
                           <option value="cancelled">Cancelled</option>
                           <option value="confirmed">Confirmed</option>
